4bdfd906eba5f6b48395df66c7291ca6c596c7bd
Andrey Vassilyev <vasiliev@a-3.ru>
2019-06-26 18:00:01 +0300

    fix build proccess

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -2,12 +2,13 @@
 const path = require('path')
 const babelPlugin = require('rollup-plugin-babel')
 const resolvePlugin = require('rollup-plugin-node-resolve')
+const externalPlugin = require('rollup-plugin-auto-external')
 
 const rootDir = path.resolve(__dirname, '..');
 
 const inputConfig = {
     input: path.resolve(rootDir, 'src', 'index.js'),
-    plugins: [babelPlugin(), resolvePlugin()],
+    plugins: [babelPlugin(), resolvePlugin(), externalPlugin()],
 }
 
 const outputConfig = {
