e3ac677a9f6260cb7858cf49bc578ca9fe2ea9e2
XaveScor <xavescor@gmail.com>
2019-07-07 13:24:56 +0300

    refactor useChatBuilder

--- src/useChatBuilder.js
+++ src/useChatBuilder.js
@@ -4,24 +4,29 @@ import * as React from 'react'
 import {ConformsForm} from './ReactForm'
 import {areEqualShallow} from './shallowEqual'
 
-function EmptyComponent<T>(a: T) {
-    return 'invalidComponent'
+function createComponent<TProps: {}>(
+    page: Page<TProps>,
+): React.ComponentType<TProps> {
+    let oldProps = page.props.getData()
+    return (props: TProps) => {
+        if (!areEqualShallow(props, oldProps)) {
+            page.props.replace(props)
+            oldProps = props
+        }
+        return <ConformsForm page={page} />
+    }
 }
+
+declare export function useChatBuilder<TProps: {}>(
+    page: Page<TProps>,
+): React.ComponentType<TProps>
+
 export function useChatBuilder<TProps: {}>(
     page: Page<TProps>
 ): React.ComponentType<TProps> {
-    const firstLoad = React.useRef(true)
-    const componentRef = React.useRef<React.ComponentType<TProps>>(EmptyComponent)
-    if (firstLoad.current) {
-        firstLoad.current = false
-        let oldProps = page.props.getData();
-        componentRef.current = (props: TProps) => {
-            if (!areEqualShallow(props, oldProps)) {
-                page.props.replace(props)
-                oldProps = props
-            }
-            return <ConformsForm page={page} />
-        }
+    const componentRef = React.useRef<React.ComponentType<TProps> | null>(null)
+    if (!componentRef.current) {
+        componentRef.current = createComponent(page)
     }
 
     return componentRef.current
