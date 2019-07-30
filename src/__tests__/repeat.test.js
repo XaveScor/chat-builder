caca3ea5de3ac4d47a9056301bffed2b0af105d0
Andrey Vassilyev <vasiliev@a-3.ru>
2019-07-30 16:50:11 +0300

    rewrite repeat test

--- src/__tests__/repeat.test.js
+++ src/__tests__/repeat.test.js
@@ -1,30 +1,32 @@
 import * as React from 'react';
-import {createPage, questionBubble, input, ConformsForm, Stop} from '..'
+import {createPage, questionBubble, input, ConformsForm, Stop, Repeat} from '..'
 import * as renderer from 'react-test-renderer'
 
 it('repeat', done => {
     const page = createPage()
+    const repeatPage = createPage()
     let count = 0
-    page.use(() => {
-        if (count === 2) {
-            return {
-                steps: [],
-                nextPage: Stop,
+    page.use({
+        steps: [
+            {
+                question: questionBubble,
+                questionProps: {
+                    question: 'question',
+                },
+                input,
             }
+        ],
+        nextPage: () => {
+            if (count === 2) {
+                return Stop
+            }
+            ++count
+            return repeatPage
         }
-        ++count;
-        return {
-            steps: [
-                {
-                    question: questionBubble,
-                    questionProps: {
-                        question: 'question',
-                    },
-                    input,
-                }
-            ],
-            nextPage: page,
-        }
+    })
+    repeatPage.use({
+        steps: [],
+        nextPage: Repeat,
     })
     let tree
     renderer.act(() => {
